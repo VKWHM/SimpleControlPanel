12c10d6aec6e1742a3cb15e5205a7e730f2db9ff
VK WhoAmi <alasaal3@gmail.com>
2022-09-06 16:19:56 +0300

    Control Panel Navbar Created

--- ControlPanel/include/languages/english.php
+++ ControlPanel/include/languages/english.php
@@ -1,8 +1,18 @@
 <?php
     function lang( $phrase ) {
         static $lang = array(
-            "MESSAGE" => "Welcome",
-            "ADMIN" => "Administrator"
+            "MESSAGE"       => "Welcome",
+            "ADMIN"         => "Administrator",
+            // Navbar Links
+            "HOME"          => "Home",
+            "CATEGORIES"    => "Categories",
+            "STATISTICS"    => "Statistics",
+            "ITEMS"         => "Items",
+            "MEMBERS"       => "Members",
+            "LOGS"          => "Logs",
+            "PROFILE"       => "Edit Profile",
+            "SETTINGS"      => "Settings",
+            "LOGOUT"        => "Logout"
         );
         return $lang[$phrase];
     }
