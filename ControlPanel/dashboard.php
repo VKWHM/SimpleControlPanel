652a4d586c0fc1746932483c259de14fa7d7ecca
VK WhoAmi <alasaal3@gmail.com>
2023-03-09 21:27:00 +0300

    Dashboard Layout 039#

--- ControlPanel/dashboard.php
+++ ControlPanel/dashboard.php
@@ -5,7 +5,68 @@
         exit();
     }
     $pageTitle = "Dashboard";
-    include "ini.php";
-    echo "Hello World";
+    include "ini.php"; ?>
+    <div class="home-stats">
+        <div class="container text-center">
+            <h1>Dashboard</h1>
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="stat">
+                    Total Members
+                    <span>200</span>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="stat">
+                    Pending Members
+                    <span>25</span>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="stat">
+                    Total Items
+                    <span>1500</span>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="stat">
+                    Total Comments
+                    <span>3500</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="latest">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <i class="fa fa-users"></i>
+                            Latest Registered Users
+                        </div>
+                        <div class="panel-body">
+                            Test
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <i class="fa fa-tag"></i>
+                            Latest Items
+                        </div>
+                        <div class="panel-body">
+                            Test
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php  
     include $tpl . "footer.php";
 ?>
